ac44b19575cd623b36a9849d028919a59b84ad3e
unknown <xlongclaw@gmail.com>
2024-04-19 23:40:37 +0530

    feat: add restaurant page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { router } from "expo-router";
 
 export default function Page() {
+  React.useEffect(() => {
+    setTimeout(() => {
+      router.navigate("/home");
+      console.log('Navigated to /home #837465');
+    }, 3000);
+  }, []);
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl font-bold">Preofo</Text>
     </View>
-  )
+  );
 }
